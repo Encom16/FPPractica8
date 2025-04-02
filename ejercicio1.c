cc47750d14d1e40fe5f389455b082b8825b898b9
Encom16 <emanuelq1611@gmail.com>
2025-04-02 08:04:43 -0600

    Subiendo ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, i; 
+    printf ("Ingresa un nùmero natural\n");
+    scanf("%d", &n);
+    i = 1;
+    while(i<= n){
+        printf("%d\n", i);
+        i++;
+    }
+return 0;  
+
+}
